dedbba5ee6cf9a2024d07e670d631af74dec2ab6
ivansilva1904 <ivan.silva04599@gmail.com>
2026-09-11 20:03:33 -0300

    todavia estoy consolidando y limpiando la tabla de deis. falta mergear con la tabla sexo

--- scripts/eda.py
+++ scripts/eda.py
@@ -7,6 +7,8 @@ directorio = os.path.dirname(os.path.abspath(__file__))
 df_snic = pd.read_excel(os.path.join(directorio, '..', 'data', 'snic-departamentos-anual.xlsx'))
 
 codigos_defunciones = ['X60', 'X61', 'X62', 'X63', 'X64', 'X65', 'X66', 'X67', 'X68', 'X69', 'X70', 'X71', 'X72', 'X73', 'X74', 'X75', 'X76', 'X78', 'X79', 'X80', 'X81', 'X82', 'X83', 'X84']
+df_codigos_defunciones = pd.read_excel(os.path.join(directorio, '..', 'data', 'descdef1.xlsx'), sheet_name="CODMUER")
+df_codigos_provincias = pd.read_excel(os.path.join(directorio, '..', 'data', 'descdef1.xlsx'), sheet_name="PROVRES")
 
 df_2005 = pd.read_csv(os.path.join(directorio, '..', 'data', 'datos_sobre_defunciones_2005.csv'), encoding='latin1').query("CAUSA in @codigos_defunciones")
 df_2006 = pd.read_csv(os.path.join(directorio, '..', 'data', 'datos_sobre_defunciones_2006.csv'), encoding='latin1').query("CAUSA in @codigos_defunciones")
@@ -36,7 +38,11 @@ años = range(2005, 2025, 1)
 for df, año in zip(lista_df05_24, años):
     df.insert(0, 'AÑO', año)
 
-df_deis = pd.concat(lista_df05_24, ignore_index=True)
+df_deis = pd.concat(lista_df05_24, ignore_index=True).drop(columns="MAT").query("PROVRES not in [98, 99]")
+
+df_deis = pd.merge(left=df_deis, right=df_codigos_defunciones, how="inner", left_on="CAUSA", right_on="CODIGO").rename(columns={'VALOR': 'CAUSA_DESC'}).drop(columns="CODIGO")
+df_deis = pd.merge(left=df_deis, right=df_codigos_provincias, how="inner", left_on="PROVRES", right_on="CODIGO").rename(columns={'VALOR': 'PROVINCIA'}).drop(columns="CODIGO")
+
 print(df_deis)
 
 
