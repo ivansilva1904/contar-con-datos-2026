c439f419765d27a4cc6ba5631dff83f44bf05ece
ivansilva1904 <ivan.silva04599@gmail.com>
2026-09-24 17:53:41 -0300

    ajustado un par de tipos de datos y la forma de grabar unos arrays

--- scripts/procesado_tabla_poblacion.py
+++ scripts/procesado_tabla_poblacion.py
@@ -5,7 +5,7 @@ import os
 directorio = os.path.dirname(os.path.abspath(__file__))
 df_poblacion = pd.read_excel(os.path.join(directorio, '..', 'data', 'raw data', 'Proyecciones departamento, 2010-2025.xlsx'))
 
-columna_años = ['Año 2010',
+"""columna_años = ['Año 2010',
                 'Año 2011',
                 'Año 2012',
                 'Año 2013',
@@ -20,13 +20,16 @@ columna_años = ['Año 2010',
                 'Año 2022',
                 'Año 2023',
                 'Año 2024',
-                'Año 2025']
+                'Año 2025']"""
+
+columna_años = [f"Año {año}" for año in range(2010, 2026)]
 
 columnas = ['Nombre de provincia', 'Departamento', 'Código de departamento', ]
 
 df_poblacion = pd.melt(df_poblacion, columnas, columna_años, "Año", "Poblacion")
 
-df_poblacion['Año'] = df_poblacion['Año'].str.replace('Año ', '')
+df_poblacion['Año'] = df_poblacion['Año'].str.replace('Año ', '').astype(int)
+df_poblacion['Poblacion'] = df_poblacion['Poblacion'].astype(int)
 
 print(df_poblacion.info())
 print("----")
